86fac9d2e42f1cd5d9d3fc3d300d0a0c9053b0c0
amirro37 <101533022+amirro37@users.noreply.github.com>
2026-01-01 12:54:44 -0500

    Refactor App component to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import DashboardLayout from "./layout/DashboardLayout.jsx";
+import Overview from "./pages/Overview.jsx";
+import Users from "./pages/Users.jsx";
+import Logs from "./pages/Logs.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Changed Logo" />
-        </a>
-        <a href="https://okta.com" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<Overview />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/logs" element={<Logs />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      </Route>
+    </Routes>
+  );
 }
+
 
 export default App
