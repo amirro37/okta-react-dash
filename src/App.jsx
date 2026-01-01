f499f27ab3cd702f7d024995bb84b5596c74ca63
am-mirro <adam.mirro@gmail.com>
2026-01-01 12:25:48 -0500

    Update App.jsx logo alt text

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Changed Logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://okta.com" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
